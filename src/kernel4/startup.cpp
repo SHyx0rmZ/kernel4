4d5ccb1c3fcf99796195a0d24e2593c358216500
Patrick Pokatilo <shyxormz@asxsoft.de>
2009-12-06 21:02:56 +0100

    Nuke: Initialize Kernel
    Signed-off-by: Patrick Pokatilo <shyxormz@asxsoft.de>

---
+++ src/kernel4/startup.cpp
@@ -0,0 +1,38 @@
+/*
+ *   ASXSoft Nuke - Operating System
+ *   Copyright (C) 2009  Patrick Pokatilo
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "multiboot.h"
+
+extern "C" void entry(struct MultibootInformationSpecification *multiboot_info_32bit);
+
+void entry(struct MultibootInformationSpecification *multiboot_info_32bit/*, GDTPointer gdt_pointer*/)
+{
+	MultibootInformation multiboot;
+
+	multiboot.flags = multiboot_info_32bit->flags;
+	multiboot.memory_lower = multiboot_info_32bit->mem_lower;
+	multiboot.memory_upper = multiboot_info_32bit->mem_upper;
+	multiboot.boot_device = multiboot_info_32bit->boot_device;
+	multiboot.cmdline = (char *)multiboot_info_32bit->cmdline;
+	multiboot.modules_count = multiboot_info_32bit->mods_count;
+	multiboot.modules_address = (MultibootModule *)multiboot_info_32bit->mods_addr;
+	multiboot.memory_length = multiboot_info_32bit->mmap_length;
+	multiboot.memory_address = (MultibootMemory *)multiboot_info_32bit->mmap_addr;
+
+	while(1);
+}
