20cae796e60216dc385da5d59876fde8d7671560
Patrick Pokatilo <shyxormz@asxsoft.de>
2009-12-06 13:20:26 +0100

    Rocket: Added boot entry that was forgotten earlier >.<
    Signed-off-by: Patrick Pokatilo <shyxormz@asxsoft.de>

---
+++ src/rocket/boot.asm
@@ -0,0 +1,42 @@
+;   ASXSoft Nuke - Operating System
+;   Copyright (C) 2009  Patrick Pokatilo
+;
+;   This program is free software: you can redistribute it and/or modify
+;   it under the terms of the GNU General Public License as published by
+;   the Free Software Foundation, either version 3 of the License, or
+;   (at your option) any later version.
+;
+;   This program is distributed in the hope that it will be useful,
+;   but WITHOUT ANY WARRANTY; without even the implied warranty of
+;   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;   GNU General Public License for more details.
+;
+;   You should have received a copy of the GNU General Public License
+;   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+extern start_rocket_engine
+global entry
+
+section .multiboot
+
+header dd 0x1BADB002
+flags dd 0x00000007
+checksum dd 0xE4524FF7
+dd 0,0,0,0,0
+mode dd 1
+width dd 80
+height dd 25
+depth dd 0
+
+section .text
+
+entry:
+mov esp, 0x300000
+
+push ebx
+call start_rocket_engine
+
+idle:
+cli
+hlt
+jmp idle
