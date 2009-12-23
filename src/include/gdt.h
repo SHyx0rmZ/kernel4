07b9ebe8501bb1ead2771ddd51ddbdf30b32c0db
Patrick Pokatilo <shyxormz@asxsoft.de>
2009-12-23 18:37:23 +0100

    Nuke: GDT stuff is almost complete
    Signed-off-by: Patrick Pokatilo <shyxormz@asxsoft.de>

--- src/include/gdt.h
+++ src/include/gdt.h
@@ -21,35 +21,128 @@
 
 #include <stdint.h>
 
-enum GDTRing
+enum GDTGranularity
 {
-	Ring0 = 0x00,
-	Ring1 = 0x20,
-	Ring2 = 0x40,
-	Ring3 = 0x60
+	Byte = 0,
+	Block = 1
 };
 
 enum GDTMode
 {
-	RealMode = 0x00,
-	LongMode = 0x02,
-	ProtectedMode = 0x04
+	RealMode = 0,
+	LongMode = 1,
+	ProtectedMode = 2
+};
+
+/*enum GDTType
+{
+	DataReadOnly = 0,
+	DataReadOnlyAccessed = 1,
+	DataReadWrite = 2,
+	DataReadWriteAccessed = 3,
+	DataReadOnlyExpandDown = 4,
+	DataReadOnlyExpandDownAccessed = 5,
+	DataReadWriteExpandDown = 6,
+	DataReadWriteExpandDownAccessed = 7,
+	CodeExecuteOnly = 8,
+	CodeExecuteOnlyAccessed = 9,
+	CodeExecuteRead = 10,
+	CodeExecuteReadAccessed = 11,
+	CodeExecuteOnlyConforming = 12,
+	CodeExecuteOnlyConformingAccessed = 13,
+	CodeExecuteReadConforming = 14,
+	CodeExecuteReadConformingAccessed = 15
+};*/
+enum GDTPresence
+{
+	NonPresent = 0,
+	Present = 1
+};
+
+enum GDTRing
+{
+	Ring0 = 0,
+	Ring1 = 1,
+	Ring2 = 2,
+	Ring3 = 3
+};
+
+enum GDTSystemType32
+{
+	TaskStateSegment16Available = 1,
+	LocalDescriptorTable32 = 2,
+	TaskStateSegment16Busy = 3,
+	CallGate16 = 4,
+	TaskGate = 5,
+	InterruptGate16 = 6,
+	TrapGate16 = 7,
+	TaskStateSegment32Available = 9,
+	TaskStateSegment32Busy = 11,
+	CallGate32 = 12,
+	InterruptGate32 = 14,
+	TrapGate32 = 15,
+	Invalid32 = 255
+};
+
+enum GDTSystemType64
+{
+	UpperHalf16 = 0,
+	LocalDescriptorTable64 = 2,
+	TaskStateSegment64Available = 9,
+	TaskStateSegment64Busy = 11,
+	CallGate64 = 12,
+	InterruptGate64 = 14,
+	TrapGate64 = 15,
+	Invalid64 = 255
+};
+
+enum GDTType
+{
+	System = 0,
+	Data = 1,
+	Code = 2
 };
 
 class GDTEntry
 {
 	public:
-		GDTEntry(GDTMode mode, uint8_t flags, uint8_t access = 10, uintptr_t base = 0, uint32_t limit = 0xFFFFFFFF);
+		//GDTEntry(GDTMode mode, uint8_t flags, uint8_t access = 10, uintptr_t base = 0, uint32_t limit = 0xFFFFFFFF);
+		GDTEntry(GDTMode mode, GDTType type, GDTRing ring, uintptr_t base = 0, uint32_t limit = 0xFFFFFFFF, GDTGranularity granularity = Block, GDTPresence presence = Present);
 		~GDTEntry();
 
-		uint8_t GetAccess();
-		uintptr_t GetBase();
-		uint8_t GetFlags();
-		uint32_t GetLimit();
-		void SetAccess(uint8_t access);
-		void SetBase(uintptr_t address);
-		void SetFlags(uint8_t flags);
-		void SetLimit(uint32_t limit);
+		uintptr_t 	GetBase();
+		GDTGranularity	GetGranularity();
+		uint32_t 	GetLimit();
+		GDTMode 	GetMode();
+		GDTPresence 	GetPresence();
+		GDTRing 	GetRing();
+		GDTSystemType32 GetSystemType32();
+		GDTSystemType64 GetSystemType64();
+		GDTType 	GetType();
+		bool 		IsCodeSegment();
+		bool 		IsConforming();
+		bool 		IsDataSegment();
+		bool 		IsExecuteOnly();
+		bool 		IsExpandDown();
+		bool 		IsReadOnly();
+		bool 		IsSystemSegment();
+		void 		SetBase(uintptr_t address);
+		bool 		SetConforming(bool conforming);
+		bool		SetExecuteOnly(bool executeonly);
+		bool 		SetExpandDown(bool expanddown);
+		void		SetGranularity(GDTGranularity granularity);
+		void 		SetLimit(uint32_t limit);
+		void 		SetMode(GDTMode mode);
+		void 		SetPresence(GDTPresence presence);
+		bool 		SetReadOnly(bool readonly);
+		void 		SetRing(GDTRing ring);
+		bool 		SetSystemType(GDTSystemType32 systemtype);
+		bool 		SetSystemType(GDTSystemType64 systemtype);
+		bool 		SetSystemType32(GDTSystemType32 systemtype);
+		bool 		SetSystemType64(GDTSystemType64 systemtype);
+		void 		SetType(GDTType type);
+		//TODO: implement
+		//char 		*ToString();
 
 	private:
 		uint16_t limit_low;
@@ -58,6 +151,21 @@ class GDTEntry
 		uint8_t access;
 		uint8_t flags_and_limit_high;
 		uint8_t base_high;
+};
+
+class GDTTable
+{
+	public:
+		GDTTable(uint8_t size, uintptr_t position);
+		~GDTTable();
+
+		GDTEntry GetEntry(uint8_t index);
+		uint8_t GetSize();
+		void SetEntry(uint8_t index, GDTEntry entry);
+
+	private:
+		uint16_t limit;
+		GDTEntry *base;
 };
 
 #endif
--- src/kernel4/gdt.cpp
+++ src/kernel4/gdt.cpp
@@ -8,7 +8,10 @@
  *   (at your option) any later version.
  *
  *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   but WITHOUT ANY WARRANTY
+{
+}
+ without even the implied warranty of
  *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *   GNU General Public License for more details.
  *
@@ -18,12 +21,285 @@
 
 #include "gdt.h"
 
-GDTEntry::GDTEntry(GDTMode mode, uint8_t flags, uint8_t access, uintptr_t base, uint32_t limit)
+/*GDTEntry::GDTEntry(GDTMode mode, uint8_t flags, uint8_t access, uintptr_t base, uint32_t limit)
 {
 	// Bogus code, to enable compilation :D
-	this->base_low = base;
-	this->flags_and_limit_high = flags;
-	this->access = access;
-	this->base_low = (uint8_t)mode;
-	this->limit_low = limit;
+	this->limit_low = limit & 0xFFFF
+{
+}
+
+	this->base_low = base & 0xFFFF
+{
+}
+
+	this->base_middle = (base >> 16) & 0xFF
+{
+}
+
+	this->access = access
+{
+}
+
+	this->flags_and_limit_high = ((flags & 0xF) << 4) | ((mode & 6) << 1) | ((limit >> 16) & 0xF)
+{
+}
+
+	this->base_high = (base >> 24) & 0xFF
+{
+}
+
+}*/
+
+GDTEntry::GDTEntry(GDTMode mode, GDTType type, GDTRing ring, uintptr_t base, uint32_t limit, GDTGranularity granularity, GDTPresence presence)
+{
+	this->base_low = (base & 0xFFFF);
+	this->base_middle = ((base >> 16) & 0xFF);
+	this->base_high = ((base >> 24) & 0xFF);
+	this->limit_low = (limit & 0xFFFF);
+	this->flags_and_limit_high = ((limit >> 16) & 0x0F) | ((mode << 5) & 0x60) | ((granularity << 7) & 0x80);
+
+	switch(type)
+	{
+		case System:
+			this->access = 0x10;
+			break;
+		case Code:
+			this->access = 0x0A;
+			break;
+		case Data:
+			this->access = 0x02;
+			break;
+	}
+
+	this->access |= ((ring << 5) & 0x60) | ((presence << 7) & 0x80);
+}
+
+GDTEntry::~GDTEntry()
+{
+}
+
+uintptr_t GDTEntry::GetBase()
+{
+	return (this->base_low | (this->base_middle << 16) | (this->base_high << 24));
+}
+
+GDTGranularity GDTEntry::GetGranularity()
+{
+	return (GDTGranularity)((this->flags_and_limit_high >> 7) & 1);
+}
+
+uint32_t GDTEntry::GetLimit()
+{
+	return (this->limit_low | ((this->flags_and_limit_high & 0x0F) << 16));
+}
+
+GDTMode GDTEntry::GetMode()
+{
+	return (GDTMode)((this->flags_and_limit_high >> 5) & 3);
+}
+
+GDTPresence GDTEntry::GetPresence()
+{
+	return (GDTPresence)((this->access >> 7) & 1);
+}
+
+GDTRing GDTEntry::GetRing()
+{
+	return (GDTRing)((this->access >> 5) & 3);
+}
+
+GDTSystemType32 GDTEntry::GetSystemType32()
+{
+	if(!this->IsSystemSegment())
+	{
+		return Invalid32;
+	}
+
+	return (GDTSystemType32)(this->access & 0x0F);
+}
+
+GDTSystemType64 GDTEntry::GetSystemType64()
+{
+	if(!this->IsSystemSegment())
+	{
+		return Invalid64;
+	}
+
+	return (GDTSystemType64)(this->access & 0x0F);
+}
+
+GDTType GDTEntry::GetType()
+{
+	if((this->access >> 4) & 1)
+	{
+		return System;
+	}
+	else if((this->access >> 3) & 1)
+	{
+		return Code;
+	}
+	else
+	{
+		return Data;
+	}
+}
+
+bool GDTEntry::IsCodeSegment()
+{
+	return (!this->IsSystemSegment() && ((this->access >> 3) & 1) == 1);
+}
+
+bool GDTEntry::IsConforming()
+{
+	return (this->IsCodeSegment() && ((this->access >> 2) & 1) == 1);
+}
+
+bool GDTEntry::IsDataSegment()
+{
+	return (!this->IsSystemSegment() && ((this->access >> 3) & 1) == 0);
+}
+
+bool GDTEntry::IsExecuteOnly()
+{
+	return (this->IsCodeSegment() && ((this->access >> 1) & 1) == 0);
+}
+
+bool GDTEntry::IsExpandDown()
+{
+	return (this->IsDataSegment() && ((this->access >> 2) & 1) == 1);
+}
+
+bool GDTEntry::IsReadOnly()
+{
+	return (this->IsDataSegment() && ((this->access >> 1) & 1) == 0);
+}
+
+bool GDTEntry::IsSystemSegment()
+{
+	return (((this->access >> 4) & 1) == 1);
+}
+
+void GDTEntry::SetBase(uintptr_t address)
+{
+	this->base_low = (address & 0xFFFF);
+	this->base_middle = ((address >> 16) & 0xFF);
+	this->base_high = ((address >> 24) & 0xFF);
+}
+
+//TODO: FIX EVERYTHING FROM HERE ON.........................................
+
+bool GDTEntry::SetConforming(bool conforming)
+{
+	if(!this->IsCodeSegment())
+	{
+		return false;
+	}
+}
+
+bool GDTEntry::SetExecuteOnly(bool executeonly)
+{
+	if(!this->IsCodeSegment())
+	{
+		return false;
+	}
+}
+
+bool GDTEntry::SetExpandDown(bool expanddown)
+{
+	if(!this->IsDataSegment())
+	{
+		return false;
+	}
+}
+
+void GDTEntry::SetGranularity(GDTGranularity granularity)
+{
+}
+
+void GDTEntry::SetLimit(uint32_t limit)
+{
+}
+
+void GDTEntry::SetMode(GDTMode mode)
+{
+}
+
+void GDTEntry::SetPresence(GDTPresence presence)
+{
+}
+
+bool GDTEntry::SetReadOnly(bool readonly)
+{
+	if(!this->IsDataSegment())
+	{
+		return false;
+	}
+}
+
+void GDTEntry::SetRing(GDTRing ring)
+{
+}
+
+bool GDTEntry::SetSystemType(GDTSystemType32 systemtype)
+{
+	if(!this->IsSystemSegment())
+	{
+		return false;
+	}
+}
+
+bool GDTEntry::SetSystemType(GDTSystemType64 systemtype)
+{
+	if(!this->IsSystemSegment())
+	{
+		return false;
+	}
+}
+
+bool GDTEntry::SetSystemType32(GDTSystemType32 systemtype)
+{
+	if(!this->IsSystemSegment())
+	{
+		return false;
+	}
+}
+
+bool GDTEntry::SetSystemType64(GDTSystemType64 systemtype)
+{
+	if(!this->IsSystemSegment())
+	{
+		return false;
+	}
+}
+
+void GDTEntry::SetType(GDTType type)
+{
+}
+
+//TODO: FIX ONLY UNTIL HERE (YES I LIED XD).................................
+
+GDTTable::GDTTable(uint8_t size, uintptr_t position)
+{
+	this->limit = (size * 8) - 1;
+	this->base = (GDTEntry *)position;
+}
+
+GDTTable::~GDTTable()
+{
+}
+
+
+GDTEntry GDTTable::GetEntry(uint8_t index)
+{
+	return this->base[index];
+}
+
+uint8_t GDTTable::GetSize()
+{
+	return (this->limit + 1) / 8;
+}
+
+void GDTTable::SetEntry(uint8_t index, GDTEntry entry)
+{
+	this->base[index] = entry;
 }
